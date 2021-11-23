175855cd8c8aee7ff9ffb865622477fbad8f9585
JuanA <juanandreslujan30@gmail.com>
2021-11-23 14:58:10 -0500

    Agregar tailwind y rutas de muestra

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import React from 'react'
+import Link from "next/link"
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-green-500">
+      <p>Pagina de index</p>
+      <Link href='/admin/usuarios'>
+      <a className='cursor-pointer'> ir a admin usuarios</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Home
